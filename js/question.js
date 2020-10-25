2cc450faaa438fc0e30f48db477ea1c0c1b265d5
Cayman Heng <hengcayman@gmail.com>
2020-10-25 13:08:18 -0400

    updated JS file with quiz questions and answers

---
+++ js/question.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        choices: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
+        answer: "The User's machine running a Web browser"
+    },
+    {
+        title: "What are variables used for in JavaScript Programs?",
+        choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        answer: "Storing numbers, dates, or other values"
+    },
+    {
+        title: "Which of the following are capabilities of functions in JavaScript?",
+        choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
+        answer: "Accept parameters"
+    },
+    {
+        title: "Which of the following is not a valid JavaScript variable name?",
+        choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+        answer: "2names"
+    },
+    {
+        title: "How does JavaScript store dates in a date object?",
+        choices: ["The number of milliseconds since January 1st, 1970", "The number of milliseconds since January 1st, 1970", "The number of seconds since Netscape's public stock offering.", "None of the above"],
+        answer: "The number of milliseconds since January 1st, 1970"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<js>", "<scripting>", "<script>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        title: "Which of the following best describes JavaScript?",
+        choices: ["A low-level programming language.", "A scripting language precompiled in the browser.", "A compiled scripting language.", "An object-oriented scripting language."],
+        answer: "An object-oriented scripting language."
+    }
+];
