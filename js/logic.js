e82e914c2a7bd5ce0d0ce96ef33bafce2152b192
Cayman Heng <hengcayman@gmail.com>
2020-10-26 14:50:51 -0400

    added a for loop function to display quiz questions after clicking the start button

--- js/logic.js
+++ js/logic.js
@@ -3,13 +3,14 @@ var startQuiz = document.getElementById("start");
 
 startQuiz.addEventListener("click", function(){
     countDown();
+    displayQuiz();
 })
 
 //Count down timer will be started when "Start Quiz" button is clicked. If user answers questions incorrectly, countdown will decrease by 10. Countdown stops when it reaches zero or when user has answered all questions. If countdown reaches zero or user has already answered all questions, display score
 var timer = document.getElementById("timer");
 
 function countDown(){
-    var timeLeft = 5;
+    var timeLeft = 75;
 
     var timeInterval = setInterval(function() {
         timer.textContent = "Time: " + timeLeft;
@@ -20,5 +21,32 @@ function countDown(){
             clearInterval(timeInterval);
         }
     }, 1000);
+}
+
+var showQuiz = document.getElementById("quiz");
+
+function displayQuiz(){
+    var output=[];
+    questions.forEach(
+        (currentQuestion, questionNumber) => {
+            var answers = [];
+
+            for (letter in currentQuestion.answers) {
+                answers.push(
+                    `<label>
+                     <input type="radio" name="question${questionNumber}" value="${letter}">
+                     ${letter} :
+                     ${currentQuestion.answers[letter]}
+                     </label>`
+                );
+            }
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                 <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+    showQuiz.innerHTML = output.join('');
+
 }
 
--- js/question.js
+++ js/question.js
@@ -1,37 +1,111 @@
+// var questions = [
+//     {
+//         title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+//         choices: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
+//         answer: "The User's machine running a Web browser"
+//     },
+//     {
+//         title: "What are variables used for in JavaScript Programs?",
+//         choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+//         answer: "Storing numbers, dates, or other values"
+//     },
+//     {
+//         title: "Which of the following are capabilities of functions in JavaScript?",
+//         choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
+//         answer: "Accept parameters"
+//     },
+//     {
+//         title: "Which of the following is not a valid JavaScript variable name?",
+//         choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+//         answer: "2names"
+//     },
+//     {
+//         title: "How does JavaScript store dates in a date object?",
+//         choices: ["The number of milliseconds since January 1st, 1970", "The number of milliseconds since January 1st, 1970", "The number of seconds since Netscape's public stock offering.", "None of the above"],
+//         answer: "The number of milliseconds since January 1st, 1970"
+//     },
+//     {
+//         title: "Inside which HTML element do we put the JavaScript?",
+//         choices: ["<js>", "<scripting>", "<script>", "<javascript>"],
+//         answer: "<script>"
+//     },
+//     {
+//         title: "Which of the following best describes JavaScript?",
+//         choices: ["A low-level programming language.", "A scripting language precompiled in the browser.", "A compiled scripting language.", "An object-oriented scripting language."],
+//         answer: "An object-oriented scripting language."
+//     }
+// ];
+
+
 var questions = [
     {
-        title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-        choices: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
-        answer: "The User's machine running a Web browser"
+        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        answers: {
+            a: "The User's machine running a Web browser", 
+            b: "The Web server", 
+            c: "A central machine deep within Netscape's corporate offices", 
+            d: "None of the above"
+        },
+        correctAnswer: "a"
     },
     {
-        title: "What are variables used for in JavaScript Programs?",
-        choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
-        answer: "Storing numbers, dates, or other values"
+        question: "What are variables used for in JavaScript Programs?",
+        answers: {
+            a: "Storing numbers, dates, or other values", 
+            b: "Varying randomly", 
+            c: "Causing high-school algebra flashbacks", 
+            d: "None of the above"
+        },
+        correctAnswer: "a"
     },
     {
-        title: "Which of the following are capabilities of functions in JavaScript?",
-        choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
-        answer: "Accept parameters"
+        question: "Which of the following are capabilities of functions in JavaScript?",
+        answers: {
+            a: "Return a value", 
+            b: "Accept parameters and Return a value", 
+            c: "Accept parameters", 
+            d: "None of the above"
+        },
+        correctAnswer: "c"
     },
     {
-        title: "Which of the following is not a valid JavaScript variable name?",
-        choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
-        answer: "2names"
+        question: "Which of the following is not a valid JavaScript variable name?",
+        answers: {
+            a: "2names", 
+            b: "_first_and_last_names", 
+            c: "FirstAndLast", 
+            d: "None of the above"
+        },
+        correctAnswer: "a"
     },
     {
-        title: "How does JavaScript store dates in a date object?",
-        choices: ["The number of milliseconds since January 1st, 1970", "The number of milliseconds since January 1st, 1970", "The number of seconds since Netscape's public stock offering.", "None of the above"],
-        answer: "The number of milliseconds since January 1st, 1970"
+        question: "How does JavaScript store dates in a date object?",
+        answers: {
+            a: "The number of milliseconds since January 1st, 1970", 
+            b: "The number of days since January 1st, 1900", 
+            c: "The number of seconds since Netscape's public stock offering.", 
+            d: "None of the above"
+        },
+        correctAnswer: "a"
     },
     {
-        title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<js>", "<scripting>", "<script>", "<javascript>"],
-        answer: "<script>"
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<js>", 
+            b: "<scripting>", 
+            c: "<script>", 
+            d: "<javascript>"
+        },
+        correctAnswer: "c"
     },
     {
-        title: "Which of the following best describes JavaScript?",
-        choices: ["A low-level programming language.", "A scripting language precompiled in the browser.", "A compiled scripting language.", "An object-oriented scripting language."],
-        answer: "An object-oriented scripting language."
+        question: "Which of the following best describes JavaScript?",
+        answers: {
+            a: "A low-level programming language.", 
+            b: "A scripting language precompiled in the browser.", 
+            c: "A compiled scripting language.", 
+            d: "An object-oriented scripting language."
+        },
+        correctAnswer: "d"
     }
 ];
